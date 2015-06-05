b1041c74b6cf8b25d4b1b12246f1ed63dd125536
James Williams <jamesrwilliams91@gmail.com>
2015-06-05 09:17:49 -0700

    added price calulator logic to pizza class

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,19 @@
 //   // body...
 // }
 //
-function Pizza(toppings, size, cost) {
+function Pizza(size, cost, toppings) {
   this.toppings = []
   this.size = size
   this.cost = cost
 }
 
-Pizza.prototype.addToppings = function(toppings) {
-  this.toppings.push(toppings)
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping)
 };
 
+// Pizza.prototype.calculateCost = function() {
+//   this.price += this.toppings.length
+// };
 
 //
 // $(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,22 @@
 describe("Pizza", function() {
   it("is initialized with set properties", function() {
-    var pizza = new Pizza([], "large", 12)
+    var pizza = new Pizza("large", 12)
     expect(pizza.size).to.equal("large")
     expect(pizza.cost).to.equal(12)
     expect(pizza.toppings).to.eql([])
 
+  });
+
+  it("can have toppings added to it", function() {
+    var pizza = new Pizza("large", 12)
+    pizza.addTopping("pepperoni")
+    expect(pizza.toppings).to.eql(["pepperoni"])
+  });
+  it("can have its price calculated", function() {
+    var pizza = new Pizza("large", 12)
+    pizza.addTopping("pepperoni")
+    pizza.addTopping("mushrooms")
+    pizza.calculateCost()
+    expect(pizza.price).to.eql(["pepperoni"])
   });
 });
