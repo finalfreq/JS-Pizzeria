e41f17d9d750887479f314bfba2aad15b9f6f9e4
James Williams <jamesrwilliams91@gmail.com>
2015-06-05 10:45:10 -0700

    add allToppings master list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+allToppings = [
+  "cheese",
+  "red onions",
+  "green onions",
+  "green peppers",
+  "anchovies",
+  "mushrooms",
+  "olives",
+  "artichokes",
+  "chicken",
+  "pepperoni",
+  "beef",
+  "salami" ]
+
 function Pizza(size, cost, toppings) {
   this.toppings = []
   this.size = size
@@ -12,7 +26,10 @@ Pizza.prototype.calculateCost = function() {
   this.cost += this.toppings.length * 1.50
 };
 
-//
-// $(function() {
-//   // body...
-// });
+
+$(function() {
+  $("button#cyo").click(function() {
+    $("#order-form").show();
+    $("#home-page").hide();
+  });
+});
